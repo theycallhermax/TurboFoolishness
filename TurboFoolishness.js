c64ae5558915968a3e93126d79750fb2c0a4c297
M.D. Walters <67456566+Mwalters75@users.noreply.github.com>
2021-12-22 16:12:54 -0500

    Reverted changes

--- TurboFoolishness.js
+++ TurboFoolishness.js
@@ -9,19 +9,12 @@ class turboFoolishness {
           opcode: 'version',
           blockType: Scratch.BlockType.REPORTER,
           text: 'TurboFoolishness Version'
-        {  
-          opcode: 'author',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'TurboFoolishness Author'
         }
       ]
     };
   }
   version() {
     return '1.0';
-  }
-  author() {
-    return 'M.D. Walters';
   }
 }
 Scratch.extensions.register(new turboFoolishness());
