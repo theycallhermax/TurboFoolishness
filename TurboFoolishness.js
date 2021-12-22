6da7e579a6a0dc8f55bc4345aea75b2a284bf670
M.D. Walters <67456566+Mwalters75@users.noreply.github.com>
2021-12-22 16:41:40 -0500

    Custom block images, anyone?

--- TurboFoolishness.js
+++ TurboFoolishness.js
@@ -1,10 +1,13 @@
-const cl_icon = 'https://mwalters75.github.io/TurboFoolishness/img/ext-img.svg';
+const vers = '1.0';
+const cl_icon = 'https://mwalters75.github.io/TurboFoolishness/img/extension.svg';
+const cl_block = 'https://mwalters75.github.io/TurboFoolishness/img/block.svg';
 class turboFoolishness {
   getInfo() {
     return {
       id: 'turbofoolishness',
       name: 'TurboFoolishness',
       menuIconURI: cl_icon,
+      blockIconURI: cl_block,
       blocks: [
         {
           opcode: 'version',
