d3a41bacfc52577dc96b0f4579b854ca059d5e7f
M.D. Walters <67456566+Mwalters75@users.noreply.github.com>
2021-12-22 01:43:03 -0500

    Removed comments

--- TurboFoolishness.js
+++ TurboFoolishness.js
@@ -6,25 +6,20 @@ class turboFoolishness {
 
       blocks: [
         {
-          opcode: 'tfver',
+          opcode: 'version',
           blockType: Scratch.BlockType.REPORTER,
+          text: 'TurboFoolishness Version'
+        }
+        {
+          opcode: 'version',
+          blockType: Scratch.BlockType.B,
           text: 'TurboFoolishness Version'
         }
       ]
     };
   }
-
-  /**
-   * Corresponds to `opcode: 'hello'` above
-   */
-  tfver() {
-    // You can just return a value: any string, boolean, or number will work.
-    // If you have to perform an asynchronous action like a request, just return a Promise.
-    // The block will wait until the Promise resolves and return the resolved value.
+  version() {
     return '1.0';
   }
 }
-
-// Call Scratch.extensions.register to register your extension
-// Make sure to register each extension exactly once
 Scratch.extensions.register(new turboFoolishness());
