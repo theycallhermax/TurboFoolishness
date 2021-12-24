eb92d5ddb9e76c4802e1a5b6c4bdbaca83fe2942
M.D. Walters <67456566+Mwalters75@users.noreply.github.com>
2021-12-24 14:49:04 -0500

    Removed the `menuIconURI` to fit in with the rest of the menus

--- TurboFoolishness.js
+++ TurboFoolishness.js
@@ -9,7 +9,7 @@ class turboFoolishness {
     return {
       id: 'turbofoolishness',
       name: 'TurboFoolishness',
-      menuIconURI: 'https://mwalters75.github.io/TurboFoolishness/img/extension.svg',
+      // menuIconURI: 'https://mwalters75.github.io/TurboFoolishness/img/extension.svg',
       blockIconURI: 'https://mwalters75.github.io/TurboFoolishness/img/block.svg',
       docsURI: 'https://github.com/Mwalters75/TurboFoolishness#the-making-of-turbofoolishnessdocumentation',
       color1: '#9500ff',
