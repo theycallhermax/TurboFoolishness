a6385e57b818718074821445d7e1afbf2e486bc3
M.D. Walters <67456566+Mwalters75@users.noreply.github.com>
2021-12-28 01:03:22 -0500

    Okay, how about without the custom colours?

--- TurboFoolishness.js
+++ TurboFoolishness.js
@@ -9,7 +9,7 @@ class turboFoolishness {
       // blockIconURI: 'https://mwalters75.github.io/TurboFoolishness/img/block.svg',
       docsURI: 'https://github.com/Mwalters75/TurboFoolishness#the-making-of-turbofoolishnessdocumentation',
       color1: '#9500ff',
-      color2: '#000000',
+      // color2: '#000000',
       blocks: [
         {
           opcode: 'version',
