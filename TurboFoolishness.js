0e9936c0380634cf099d7d0f834029b16b6c9463
M.D. Walters <67456566+Mwalters75@users.noreply.github.com>
2021-12-29 21:28:04 -0500

    Re-added a feature

--- TurboFoolishness.js
+++ TurboFoolishness.js
@@ -93,8 +93,20 @@ class turboFoolishness {
             }
           }
         },  {
-          opcode: '',
-        }
+          opcode: 'transfer',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'transfer [text]',
+          "arguments": {
+            "text": {
+              "type": "string",
+              "defaultValue": "foo",
+            }
+          }
+        },  {
+          opcode: 'reciever',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'reciever',
+        },
       ]
     };
   }
@@ -121,6 +133,12 @@ dummyboolean() {
   };
 fetch({url}) {
     return fetch(url).then(response => response.text())
+  };
+transfer({text}) {
+    const transfer = args.text;
+  };
+reciever() {
+    return transfer;
   };
 }
 Scratch.extensions.register(new turboFoolishness());
