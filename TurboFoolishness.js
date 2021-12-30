9b4ad32f0ad641723d29ad3f2c46d0cd22bafc83
M.D. Walters <67456566+Mwalters75@users.noreply.github.com>
2021-12-30 15:42:49 -0500

    Removed dummy blocks, and moved them into a archive folder.

--- TurboFoolishness.js
+++ TurboFoolishness.js
@@ -32,47 +32,6 @@ class turboFoolishness {
           blockType: Scratch.BlockType.REPORTER,
           text: 'eat'
         },  {
-          opcode: 'dummy-hat',
-          blockType: Scratch.BlockType.HAT,
-          text: 'dummy hat',
-        },  {
-          opcode: 'dummy-block',
-          blockType: Scratch.BlockType.COMMAND,
-          text: 'dummy block',
-        },  {
-          opcode: 'dummyreporter',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'dummy reporter',
-        },  {
-          opcode: 'dummyboolean',
-          blockType: Scratch.BlockType.BOOLEAN,
-          text: 'dummy boolean'
-        },  {
-          opcode: 'dummy-conditional-one',
-          blockType: Scratch.BlockType.CONDITIONAL,
-          text: 'dummy conditional with one branch',
-          branchCount: 1,
-        },  {
-          opcode: 'dummy-conditional-two',
-          blockType: Scratch.BlockType.CONDITIONAL,
-          text: 'dummy conditional with two branches',
-          branchCount: 2,
-        },  {
-          opcode: 'dummy-conditional-three',
-          blockType: Scratch.BlockType.CONDITIONAL,
-          text: 'dummy conditional with three branches',
-          branchCount: 3,
-        },  {
-          opcode: 'dummy-conditional-four',
-          blockType: Scratch.BlockType.CONDITIONAL,
-          text: 'dummy conditonal with four branches',
-          branchCount: 4,
-        },  {
-          opcode: 'dummy-conditional-five',
-          blockType: Scratch.BlockType.CONDITIONAL,
-          text: 'dummy conditional with five branches',
-          branchCount: 5,
-        },  {
           opcode: 'fetch',
           blockType: Scratch.BlockType.REPORTER,
           text: 'fetch [url]',
