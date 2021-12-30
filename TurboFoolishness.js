b2ee886da4cda74efca95c9ba89a6e010e176348
M.D. Walters <67456566+Mwalters75@users.noreply.github.com>
2021-12-30 15:25:04 -0500

    Added a description

--- TurboFoolishness.js
+++ TurboFoolishness.js
@@ -8,6 +8,7 @@ class turboFoolishness {
       menuIconURI: 'https://mwalters75.github.io/TurboFoolishness/img/extension.svg',
       blockIconURI: 'https://mwalters75.github.io/TurboFoolishness/img/block.svg',
       docsURI: 'https://github.com/Mwalters75/TurboFoolishness#the-making-of-turbofoolishnessdocumentation',
+      description: 'No wonder why this extension is called "TurboFoolishness".',
       color1: '#9500ff',
       color2: '#000000',
       blocks: [
