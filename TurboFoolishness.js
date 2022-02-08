2475c6c1bf18f016cce065607e5a2cab8c8cc5de
M.D. Walters <67456566+Mwalters75@users.noreply.github.com>
2022-02-07 20:48:06 -0500

    Forgot :(

--- TurboFoolishness.js
+++ TurboFoolishness.js
@@ -1,8 +1,6 @@
 class turboFoolishness {
   constructor (runtime) {
     /**
-     * Store this for later communication with the Scratch VM runtime.
-     * If this extension is running in a sandbox then `runtime` is an async proxy object.
      * @type {Runtime}
      */
     this.runtime = runtime;
