c2d2d54128803f0ece3414aed4faf95a134baa28
M.D. Walters <67456566+Mwalters75@users.noreply.github.com>
2022-02-07 20:41:04 -0500

    Added VM accessing (I think)

--- TurboFoolishness.js
+++ TurboFoolishness.js
@@ -1,4 +1,12 @@
 class turboFoolishness {
+  constructor (runtime) {
+    /**
+     * Store this for later communication with the Scratch VM runtime.
+     * If this extension is running in a sandbox then `runtime` is an async proxy object.
+     * @type {Runtime}
+     */
+    this.runtime = runtime;
+}
   getInfo() {
     return {
       id: 'turbofoolishness',
