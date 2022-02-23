d95c06c8b8539dd3088111a603742e0243f83558
Mwalters75 <808426@pdsb.net>
2022-02-23 12:41:28 -0500

    Remove stringify

--- TurboFoolishness.js
+++ TurboFoolishness.js
@@ -187,26 +187,6 @@ class turboFoolishness {
               "defaultValue": "Foobar"
             }
           }
-        },  '---',  {
-          opcode: 'json_parse',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'parse [string]',
-          "arguments": {
-            "string": {
-              "type": Scratch.ArgumentType.STRING,
-              "defaultValue": '{ "foo": "foobar" }'
-            }
-          }
-        },  {
-          opcode: 'json_stringify',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'stringify [string]',
-          "arguments": {
-            "string": {
-              "type": Scratch.ArgumentType.STRING,
-              "defaultValue": '{ "foo": "foobar" }'
-            }
-          }
         },
       ]
     }  
@@ -296,16 +276,6 @@ stringtoboolean(args) {
   };
 strict_equality(args) {
   return (args.sentance === args.other_sentance);
-  };
-json_parse(args) {
-  const json = args.string;
-  const obj = JSON.parse(json);
-  return obj;
-  };
-json_parse(args) {
-  const json = args.string;
-  const obj = JSON.stringify(json);
-  return obj;
   };
 }
 Scratch.extensions.register(new turboFoolishness());
